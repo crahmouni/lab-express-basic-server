d8c7c1fea600f9cdbbe468fc8a048345552378b8
Chahinaz <crahmouni@uoc.edu>
2025-03-20 10:20:20 +0100

    done

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 // IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morgan = require("morgan");
 
 // CREATE EXPRESS APP
-// Here you should create your Express app:
-
-
+const app = express();
 
 // MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-
+app.use(express.static("public")); // Servir archivos estáticos
+app.use(express.json()); // Parsear JSON
+app.use(morgan("dev")); // Registrar peticiones
 
 // ROUTES
-// Start defining your routes here:
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
 
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
 
+app.get("/api/projects", (req, res) => {
+  const projects = require("./data/projects.json");
+  res.json(projects);
+});
+
+app.get("/api/articles", (req, res) => {
+  const articles = require("./data/articles.json");
+  res.json(articles);
+});
+
+// 404 ROUTE - Si ninguna ruta coincide, devuelve la página de error 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/views/not-found.html");
+});
 
 // START THE SERVER
-// Make your Express server listen on port 5005:
+app.listen(5005, () => {
+  console.log("Server listening on port 5005");
+});
